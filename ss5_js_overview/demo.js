bb06a798e4399928a9cb3c2d6e7d278883547e43
haitan2408 <haitan28102408@gmail.com>
2024-07-08 10:09:42 +0700

    haitt up demo ss5

---
+++ ss5_js_overview/demo.js
@@ -0,0 +1,14 @@
+
+/*
+Với lệnh log thì ....
+Với lệnh alert ....
+Creator: HaiTT
+Content: xây dựng hàm hiển thị với 2 cách log và alert
+Input
+Output
+Version: 1.0
+*/
+function hello() {
+    console.log("123")
+    alert("456")
+}
